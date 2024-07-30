0af7317cea108f72ab17e2ed4470b336995a2413
MBrock_29 <mbrock19@gmail.com>
2024-07-30 14:38:21 +0100

    writing contract

---
+++ smart-contracts/src/Subscription.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Subscription {
+    uint256 subscriptionAmount = 5e18;
+
+    // constructor() payable {
+    //     subscriptionAmount = 5e18;
+    // }
+
+    event Subscribed(
+        address indexed user,
+        address indexed streamer,
+        uint256 weiAmount,
+        uint256 contractBalance
+    );
+
+    function subscribe(address streamer) public payable {
+        require(msg.value == subscriptionAmount, "Incorrect subscription fee");
+        payable(streamer).transfer(msg.value);
+
+        emit Subscribed(msg.sender, streamer, msg.value, address(this).balance);
+    }
+}
